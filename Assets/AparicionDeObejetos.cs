8467dd6089532f699216483e493c306ac3094817
Pepe08tn <48521743@est.ort.edu.ar>
2024-06-26 15:01:01 -0300

    Probando Script spawnear objetos

--- Assets/AparicionDeObejetos.cs
+++ Assets/AparicionDeObejetos.cs
@@ -9,13 +9,15 @@ public class AparicionDeObejetos : MonoBehaviour
     public int CantidadAparecidos;
     public int MaxSpawn;
     public int MinSpawn;
-    public int RandomSpawn;
+    public Transform Spawner;
+    public int RandomSpawn-40;
 
     // Start is called before the first frame update
     void Start()
     {
         CantidadObjetos = Random.Range(MinSpawn, MaxSpawn + 1);
         CantidadObjetos = CantidadAparecidos;
+        SpawnearObjetos();
     }
 
     // Update is called once per frame
@@ -26,7 +28,12 @@ public class AparicionDeObejetos : MonoBehaviour
 
     public void SpawnearObjetos()
     {
+        CantidadAparecidos--;
         int SeleccionRandomObject = Random.Range(0, objetos.Length);
         float PositionRandomX = Random.Range(-RandomSpawn, RandomSpawn);
+        float PositionRandomZ = Random.Range(-RandomSpawn, RandomSpawn);
+
+        Vector3 posicion = new Vector3(PositionRandomX, Spawner.position.y, PositionRandomZ);
+        Instantiate(objetos[SeleccionRandomObject], posicion, Quaternion.identity);
     }
 }
