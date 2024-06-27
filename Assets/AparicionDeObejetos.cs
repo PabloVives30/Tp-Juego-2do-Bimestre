fd74cd23485c84f41017e6a16b0335135efcea1f
PabloVives30 <48116412@est.ort.edu.ar>
2024-06-27 09:52:14 -0300

    cambios

--- Assets/AparicionDeObejetos.cs
+++ Assets/AparicionDeObejetos.cs
@@ -14,12 +14,17 @@ public class AparicionDeObejetos : MonoBehaviour
     public float Limites;
     public Button responderbtn;
     public InputField inputFieldcantidad;
+    int cantidadpantalla;
+    int queobjeto;
+    public GameObject PanelGanaste;
+    public GameObject PanelPerdiste;
 
     void Start()
     {
-        CantidadObjetos = Random.Range(minObjects, maxObjects + 1);
-        CantidadObjetosEnPantalla = CantidadObjetos;
-        for (int i = 0; i < CantidadObjetos; i++)
+        CantidadObjetos = Random.Range(minObjects, maxObjects +1);
+        
+        queobjeto = Random.Range(0, objects.Length);
+        cantidadpantalla = CantidadObjetos;
             InstanciarObjetos();
     }
 
@@ -29,19 +34,36 @@ public class AparicionDeObejetos : MonoBehaviour
     }
 
     public void InstanciarObjetos()
-    {
-        int i = Random.Range(0, objects.Length);
+    { 
         float PosicionX = Random.Range(-11.31f, 8.47f);
         float PosicionZ = Random.Range(-5.9f, 17.5f);
         Vector3 randomSpanwpoint = new Vector3(PosicionX, Spawn.position.y, PosicionZ);
-        Instantiate(objects[i], randomSpanwpoint, Quaternion.identity);
+        Instantiate(objects[queobjeto], randomSpanwpoint, Quaternion.identity);
+
+        if (cantidadpantalla >= 0)
+        {
+            cantidadpantalla--;
+            Invoke(nameof(InstanciarObjetos), 1f);
+        }
     }
 
     public void botonresponder()
     {
-        if(inputFieldcantidad.text == "")
+        if (inputFieldcantidad.text == "")
         {
             Debug.Log("Ingrese un número por favor");
         }
+        else
+        {
+            if (int.Parse(inputFieldcantidad.text) == CantidadObjetos)
+            {
+                PanelGanaste.SetActive(true);
+            }
+            else
+            {
+                PanelPerdiste.SetActive(true);
+            }
+        }
     }
+
 }
